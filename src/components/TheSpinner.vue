e8337e1c36dc37106cbd62232d24131f90bbbbd5
gtopsis <csd3004@csd.uoc.gr>
2025-04-24 22:06:45 +0300

    feat(TheSpinner): add component to visualize animated icon for loading states

---
+++ src/components/TheSpinner.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size?: 'sm' | 'md' | 'lg'
+  }>(),
+  { size: 'md' },
+)
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div
+      class="border-grey animate-spin rounded-full border-4 border-t-transparent"
+      :class="{
+        'h-4 w-4': size === 'sm',
+        'h-8 w-8': size === 'md',
+        'h-12 w-12': size === 'lg',
+      }"
+    ></div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
