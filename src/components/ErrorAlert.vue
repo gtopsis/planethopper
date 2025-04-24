a6dfce61369454f04db9afbe35020b56b78de6e7
gtopsis <csd3004@csd.uoc.gr>
2025-04-24 22:08:08 +0300

    feat(ErrorAlert): create component to display error messages in a horizontal panel

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  error: Error | null
+}
+const { error } = defineProps<Props>()
+
+const message = computed(() => error || 'Something wrong happened. Please try again!')
+</script>
+
+<template>
+  <div v-if="error" class="border-error text-error border-l-4 bg-orange-100 p-4" role="alert">
+    <h3 class="text-lg font-bold">An error occurred</h3>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
