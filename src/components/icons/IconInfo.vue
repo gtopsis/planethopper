db4b62cd7ce1c29c68c290de14365cd083108841
gtopsis <csd3004@csd.uoc.gr>
2025-04-25 07:22:10 +0300

    feat(IconInfo): allow size customization

--- src/components/icons/IconInfo.vue
+++ src/components/icons/IconInfo.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  size?: 'sm' | 'md' | 'lg'
+}
+withDefaults(defineProps<Props>(), { size: 'md' })
+
+const sizeInPixels = {
+  sm: 16,
+  md: 20,
+  lg: 24,
+}
+</script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    :width="sizeInPixels[size]"
+    :height="sizeInPixels[size]"
+    fill="currentColor"
+  >
     <path
       fill-rule="evenodd"
       d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
