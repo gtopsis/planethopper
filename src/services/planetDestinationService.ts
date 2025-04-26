b68eef4542d6c3261f822e6b3e84dbff759ddab9
gtopsis <csd3004@csd.uoc.gr>
2025-04-26 06:37:22 +0300

    feat(planetDestinationService): implement planet destination service
    - Responsible for managing planet destination data coming from API, like populating extra data on each planet destination.

---
+++ src/services/planetDestinationService.ts
@@ -0,0 +1,35 @@
+import { appConfig } from '@/appConfig'
+import { type PlanetDestinationData, type PlanetDestinationExtended } from '@/types'
+import { getRandomIntegersInRange } from '@/utils'
+
+export const planetDestinationService = () => {
+  const destinationImages = ['Skiathos', 'Gavdos', 'Naxos', 'Ios']
+  const planetImageApiUrl = new URL(appConfig.TRIP_DESTINATION_IMAGE_API_URL)
+
+  const getRandomImageUrl = (): string => {
+    const randomImage = destinationImages[getRandomIntegersInRange(0, destinationImages.length - 1)]
+    const imageUrl = new URL(`${randomImage}.jpg`, planetImageApiUrl)
+
+    return imageUrl.toString()
+  }
+
+  return {
+    createPaginatedApiUrlString(page = 1, baseUrl = appConfig.TRIP_DESTINATION_API_URL): string {
+      const url = new URL(baseUrl)
+      url.searchParams.append('page', page.toString())
+
+      return url.toString()
+    },
+
+    populatePlanetDestinations(
+      planetDestinations: PlanetDestinationData[],
+    ): PlanetDestinationExtended[] {
+      return planetDestinations.map((pd) => ({
+        ...pd,
+        id: pd.url || pd.name,
+        imageUrl: getRandomImageUrl(),
+        isSelected: false,
+      }))
+    },
+  }
+}
