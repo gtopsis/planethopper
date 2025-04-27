6df9274f3dcb7c7de62a93ab1d92b6e39201e386
gtopsis <csd3004@csd.uoc.gr>
2025-04-27 23:09:24 +0300

    feat(TripPlanManager): create TripPlanManager component to manage and display the trip plan

---
+++ src/components/TripPlanManager.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TripPlanList from '@/components/TripPlanList.vue'
+import IconClear from '@/components/icons/iconClear.vue'
+import { ref } from 'vue'
+import BaseButton from './BaseButton.vue'
+
+const tripPlanItems = ref([
+  { name: 'giorgos', id: '1' },
+  { name: 'aris', id: '2' },
+])
+
+const removeAllTripPlanItems = () => {
+  tripPlanItems.value = []
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <p class="mb-1 px-2 py-1 text-center font-bold">
+      <span>Your planetary route</span>
+    </p>
+
+    <TripPlanList :trip-plan-items="tripPlanItems" />
+
+    <BaseButton
+      v-if="tripPlanItems.length > 0"
+      class="mt-2"
+      theme="error"
+      aria-label="Load more destinations to planets"
+      @click="removeAllTripPlanItems"
+    >
+      <IconClear class="mr-2" size="sm" />
+      <span>Clear list</span>
+    </BaseButton>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
