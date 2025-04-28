e9c67cf422389c3c64b2f64ba8890b8d388b949e
gtopsis <csd3004@csd.uoc.gr>
2025-04-28 11:18:18 +0300

    feat(HomePage): integrate PlanetDestinationManager and TripPlanManager components

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,29 +1,34 @@
 <script setup lang="ts">
+import PlanetDestinationManager from '@/components/PlanetDestinationManager.vue'
+import TripPlanManager from '@/components/TripPlanManager.vue'
+
 const appName = 'Planet Hopper'
 </script>
 
 <template>
-  <div class="prose lg:prose-xl font-primary bg-background grid min-h-screen grid-rows-[auto_1fr]">
-    <header class="bg-background sticky top-0 p-4 text-left md:text-center">
-      <h1 class="text-text-primary text-2xl font-bold">{{ appName }}</h1>
+  <div class="prose lg:prose-xl font-primary bg-background flex h-screen flex-col overflow-y-auto">
+    <header
+      class="bg-background sticky top-0 z-10 h-[60px] w-full shrink-0 p-4 text-left md:h-[70px] md:text-center"
+    >
+      <h1 class="text-text-primary text-2xl font-bold md:text-3xl">{{ appName }}</h1>
     </header>
 
-    <div class="main-container container mx-auto md:px-4">
-      <div class="grid gap-2 md:grid-cols-12">
+    <div class="container mx-auto md:px-4">
+      <div class="grid h-full gap-2 rounded-2xl md:grid-cols-12">
         <main
-          class="bg-surface order-2 p-4 md:order-1 md:col-span-8 md:rounded-lg md:shadow"
-        ></main>
+          class="order-2 h-full overflow-y-auto rounded-2xl p-2 md:order-1 md:col-span-8 md:rounded-lg md:shadow lg:col-span-9"
+        >
+          <PlanetDestinationManager />
+        </main>
 
         <aside
-          class="bg-surface sticky top-0 order-1 p-4 md:order-2 md:col-span-4 md:rounded-lg md:shadow"
-        ></aside>
+          class="bg-primary sticky top-15 order-1 max-h-[350px] p-4 md:top-17 md:order-2 md:col-span-4 md:h-[calc(100vh-75px)] md:rounded-lg md:shadow lg:col-span-3"
+        >
+          <TripPlanManager />
+        </aside>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="css" scoped>
-.main-container {
-  margin: 0 auto;
-}
-</style>
+<style lang="css" scoped></style>
